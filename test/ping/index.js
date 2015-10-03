d7ec33f6d5eb37967e9f2822e452a9fbd360d564
Hendra G <drabiter@gmail.com>
2015-10-03 18:03:05 +0700

    add /ping

--- test/ping/index.js
+++ test/ping/index.js
@@ -9,34 +9,31 @@ var kraken = require('kraken-js'),
     request = require('supertest');
 
 
-describe('index', function () {
+describe('ping', function () {
 
     var app, mock;
-
 
     beforeEach(function (done) {
         app = express();
         app.on('start', done);
         app.use(kraken({
-            basedir: path.resolve(__dirname, '..')
+            basedir: path.resolve(__dirname, '../..')
         }));
 
         mock = app.listen(1337);
 
     });
 
-
     afterEach(function (done) {
         mock.close(done);
     });
 
-
-    it('should say "hello"', function (done) {
+    it('should say "pong"', function (done) {
         request(mock)
-            .get('/')
+            .get('/ping')
             .expect(200)
             .expect('Content-Type', /html/)
-                .expect(/Hello, /)
+                .expect(/pong/)
             .end(function (err, res) {
                 done(err);
             });
